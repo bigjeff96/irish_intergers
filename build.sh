bcbe0d0fab44d936bea12defecd17bb16c614fed
bigjeff96 <joepep271996@gmail.com>
2023-05-25 11:25:58 +0200

    is it working?

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-
+ODIN="~/Dropbox/Projects/odin/odin-ubuntu-amd64-dev-2023-05/./odin"
 set -e
 echo BUILD:
-time odin build . -debug -use-separate-modules 
+time odin build . -debug -use-separate-modules
 echo OUTPUT:
-./irish_integers.bin 
+./irish_integers.bin
